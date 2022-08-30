7796129d67163b1c5a8c41bf8d46a80dccce8a33
DESKTOP-4OK39MV\user <>
2022-08-30 16:55:52 +0900

    [0830] spring project - jsp, DB 기본 설정 완료 - minjeong073/SpringProject_Donggram#1 이슈 정리

--- src/main/java/com/ming/project/donggram/SpringProjectDonggramApplication.java
+++ src/main/java/com/ming/project/donggram/SpringProjectDonggramApplication.java
@@ -1,11 +1,9 @@
 package com.ming.project.donggram;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
 @SpringBootApplication
 public class SpringProjectDonggramApplication {
 
